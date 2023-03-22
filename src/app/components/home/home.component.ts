d4e520f148ca7d7a5f5c3b0a78dd1767192a5433
Gersson123 <a3521110670@alumno.uttehuacan.edu.mx>
2023-03-21 22:30:56 -0600

    se agrega proyecto

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,20 @@
+import { AuthService } from './../../services/auth.service';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  constructor(private router:Router) { }
+
+  ngOnInit(): void {
+  }
+
+  navegar(){
+    this.router.navigate(['/pagina1'])
+  }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyDPk6t0E2FrQ8-c0YvXMB8gRE24U49Kk9c",
+    authDomain: "login2-77f82.firebaseapp.com",
+    projectId: "login2-77f82",
+    storageBucket: "login2-77f82.appspot.com",
+    messagingSenderId: "126158324411",
+    appId: "1:126158324411:web:e06ffbbcf413f139ab5f9e"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
